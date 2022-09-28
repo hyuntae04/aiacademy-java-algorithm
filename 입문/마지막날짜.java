7619d65f00e46bd4649ccaa51915d4afe0d60325
hyuntae04 <shinlims2004@naver.com>
2022-09-28 17:56:44 +0900

    first commit

--- 입문/마지막날짜.java
+++ 입문/마지막날짜.java
@@ -1,5 +1,7 @@
 package 입문;
 
+import java.util.Scanner;
+
 class 마지막날짜{
     public static void main(String[] args) { 
         마지막날짜 soluction = new 마지막날짜();
@@ -8,9 +10,20 @@ class 마지막날짜{
     }
     public String solution()
     {
+        Scanner scanner = new Scanner(System.in);
         String title = "마지막 날짜";
         String answer = "";
-        return title + answer;
+        System.out.println("년");
+        int year = scanner.nextInt();
+        System.out.println("월");
+        int month = scanner.nextInt();
+        System.out.println("일");
+        int day = scanner.nextInt();
+        if(year % 4 == 0 && year % 100 != 0 && year % 400 == 0)
+        {
+            answer = "윤년입니다";
+        }
+        return title + answer;  
     }
 }
 
--- 입문/비만도측정.java
+++ 입문/비만도측정.java
@@ -13,7 +13,7 @@ class 비만도측정 {
         String name = "";
         float height = 0.0f;
         float kg = 0.0f;
-        int bmi = 0;
+        float bmi = 0;
         String degree = "";
         Scanner scanner = new Scanner(System.in);
         System.out.println("이름 입력 : ");
@@ -22,11 +22,18 @@ class 비만도측정 {
         height = scanner.nextFloat();
         System.out.println("몸무게 입력 : ");
         kg = scanner.nextFloat();
+        float m = 0.0f;
+        m = height *100;
+        bmi= kg/m;
+        if(bmi > 30){degree = "비만";}
+        else if(25<=bmi) {degree = "과체중";}
+        else if(19<=bmi) {degree = "정상";}
+        else {degree = "저체중";}
         String answer = String.format(
         "***********************************************************************************\n"
         +"*번호 이름 키 체충 BMI 비만 정도\n"
         +"************************************************************************************\n"
-        +"%s %f %f %d %s 정상\n"
+        +"%s %f %f %f %s 정상\n"
         +"************************************************************************************\n"
         +"비만 : 3 과체중 : 2 정상 : 3 저체중 : 2",
         name, height, kg, bmi, degree);
