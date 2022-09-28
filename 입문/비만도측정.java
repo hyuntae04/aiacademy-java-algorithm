29ae50f8d67b50e946d637955b64bafe6257630e
hyuntae04 <shinlims2004@naver.com>
2022-09-28 16:29:09 +0900

    first commit

---
+++ 입문/비만도측정.java
@@ -0,0 +1,35 @@
+package 입문;
+
+import java.util.Scanner;
+
+class 비만도측정 {
+    public static void main(String[] args) {
+        비만도측정 solution = new 비만도측정();
+        System.out.println(solution.solution());
+    }
+    public String solution()
+    {
+        String title = "비만도측정";
+        String name = "";
+        float height = 0.0f;
+        float kg = 0.0f;
+        int bmi = 0;
+        String degree = "";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("이름 입력 : ");
+        name = scanner.next();
+        System.out.println("키 입력 : ");
+        height = scanner.nextFloat();
+        System.out.println("몸무게 입력 : ");
+        kg = scanner.nextFloat();
+        String answer = String.format(
+        "***********************************************************************************\n"
+        +"*번호 이름 키 체충 BMI 비만 정도\n"
+        +"************************************************************************************\n"
+        +"%s %f %f %d %s 정상\n"
+        +"************************************************************************************\n"
+        +"비만 : 3 과체중 : 2 정상 : 3 저체중 : 2",
+        name, height, kg, bmi, degree);
+        return title + answer;
+    }
+}
